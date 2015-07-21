b64b09414b3e9cf84799e229329d5c7fd166ea94
Takahiro OKUMURA <hfm.garden@gmail.com>
2015-07-21 09:00:47 +0900

    wip

--- lib/itamae/plugin/resource/tar/extract.rb
+++ lib/itamae/plugin/resource/tar/extract.rb
@@ -3,17 +3,24 @@ require 'itamae'
 module Itamae
   module Plugin
     module Resource
-      class Tar < Itamae::Resource::Base
+      class TarExtract < Itamae::Resource::Base
         define_attribute :action, default: :extract
         define_attribute :destination,   type: String, default_name: true
         define_attribute :source, type: String, required: true
-        define_attribute :checksum,      type: String
         define_attribute :download_dir,  type: String
-        define_attribute :compress_char, type: String, :default => 'z'
-        define_attribute :tar_flags,     type: [String, Array], :default => Array.new
+        # define_attribute :tar_flags,     type: [String, Array], :default => Array.new
+        # define_attribute :checksum,      type: String
 
-        def action_extract
-          puts "hello"
+        def action_extract(options)
+          command = %W(tar xf #{attributes.source} -C #{attributes.destination})
+          run_command(cmd)
+        end
+
+        private
+        def ensure_tar_available
+          unless run_command('which tar', error: false).exit_status == 0
+            raise '`tar` command is not available.'
+          end
         end
       end
     end
